b6026e344a5ec45bcc5d5112ec802658e9e2a8e1
stao <werewolf_st@hotmail.com>
2024-07-26 14:33:25 +0800

    添加固件唤醒词修改接口

--- http_main.py
+++ http_main.py
@@ -1,7 +1,12 @@
+import os
+import time
+
 import uvicorn
 import json
+import requests
 
 from fastapi import FastAPI
+from fastapi.background import BackgroundTasks
 from pydantic import BaseModel
 from starlette.middleware.cors import CORSMiddleware
 from dotenv import load_dotenv
@@ -14,7 +19,6 @@ load_dotenv()
 from utils.aily_ctl import AilyCtl
 from utils.config_ctl import ConfigCtl
 from utils.device_ctl import DeviceCtl
-
 
 aily_ctl = AilyCtl()
 conf_ctl = ConfigCtl()
@@ -42,6 +46,10 @@ class ModelDataUpdate(BaseModel):
     ttsRole: Optional[str] = ""
 
 
+class AsrReqData(BaseModel):
+    wakeKeyword: str
+
+
 app = FastAPI()
 
 # 配置跨域相关策略
@@ -63,8 +71,8 @@ async def get_ping():
 
 @app.get(f"{API_PREFIX}/logs")
 async def get_logs(
-    page: int = 1,
-    perPage: int = 10,
+        page: int = 1,
+        perPage: int = 10,
 ):
     aily_ctl.log_cur_page = page
     res = aily_ctl.get_logs(perPage)
@@ -121,7 +129,7 @@ async def get_model_data():
 
 @app.post(f"{API_PREFIX}/modelData")
 async def set_model_data(
-    data: ModelDataUpdate,
+        data: ModelDataUpdate,
 ):
     aily_ctl.set_llm_url(data.llmURL)
     aily_ctl.set_llm_model(data.llmModel)
@@ -139,6 +147,92 @@ async def set_model_data(
     aily_ctl.save("reload")
 
     return ResponseModel()
+
+
+def req_gen_firmware(data: dict):
+    server = os.environ.get("FIRMWARE_SERVER")
+    url = f"{server}/api/v1/asr"
+    # 发起生成请求
+    res = requests.post(url, json=data)
+    if res.status_code != 200:
+        logger.error(f"生成固件失败: {res.text}")
+        return None
+
+    req_data = res.json()
+    gen_id = req_data["data"]["id"]
+
+    return gen_id
+
+
+def req_gen_status(gen_id: str):
+    server = os.environ.get("FIRMWARE_SERVER")
+    url = f"{server}/api/v1/firmware/status?prj_name={gen_id}"
+    # 发起状态获取请求
+    res = requests.get(url)
+    if res.status_code != 200:
+        logger.error(f"获取固件生成状态失败: {res.text}")
+        return None
+
+    req_data = res.json()
+    return req_data["data"]["status"]
+
+
+def req_download_firmware(gen_id: str):
+    server = os.environ.get("FIRMWARE_SERVER")
+    url = f"{server}/api/v1/firmware/download?prj_name={gen_id}"
+    # 发起下载请求
+    res = requests.get(url)
+    if res.status_code != 200:
+        logger.error(f"下载固件失败: {res.text}")
+        return None
+
+    # 保存固件
+    firmware_path_root = os.environ.get("FIRMWARE_PATH")
+    save_path = f"{firmware_path_root}/{gen_id}.bin"
+    with open(save_path, "wb") as f:
+        f.write(res.content)
+
+    return save_path
+
+
+def get_firmware(gen_id: str):
+    # 等待生成完成
+    gen_result = False
+    while True:
+        time.sleep(5)
+        gen_status = req_gen_status(gen_id)
+        if gen_status == 3:
+            logger.error(f"生成固件失败: {gen_id}")
+            break
+        elif gen_status == 2:
+            logger.success(f"生成固件成功: {gen_id}")
+            gen_result = True
+            break
+        else:
+            continue
+
+    if gen_result:
+        # 下载固件
+        download_path = req_download_firmware(gen_id)
+        return download_path
+    return None
+
+
+@app.post(f"{API_PREFIX}/asr")
+async def post_asr(
+        data: AsrReqData,
+        background_tasks: BackgroundTasks,
+):
+    gen_id = req_gen_firmware(data.dict())
+    background_tasks.add_task(get_firmware, gen_id)
+    return ResponseModel(data={"id": gen_id})
+
+
+@app.get(f"{API_PREFIX}/asr/status")
+async def get_asr_status(
+        prj_name: str,
+):
+    return ResponseModel(data=req_gen_status(prj_name))
 
 
 uvicorn.run(app, host="0.0.0.0", port=8888)
